60462bd9628f03c9eef73138f7c65e4ea726d5b5
kanai <34500707+kanai1@users.noreply.github.com>
2022-12-29 15:28:13 +0900

    Update view.php

--- view.php
+++ view.php
@@ -21,7 +21,7 @@
 				echo $heredoc;
 			}
 
-			$conn = mysqli_connect('localhost', 'TeamA', 'TeamA1234567@', 'test');
+			$conn = mysqli_connect('localhost', 'kknock', 'kknock', 'test');
 			$stmt = mysqli_stmt_init($conn);
 			mysqli_stmt_prepare($stmt, "SELECT * FROM board WHERE post_num=?");
 			mysqli_stmt_bind_param($stmt, 'i', $post_num);
@@ -74,91 +74,6 @@
 			}
 		?>
 		<p> <?php echo nl2br($result['body']) ?></p>
-		<?php
-			$filedir = '/var/fileupload/'.$post_num;
-			if(is_dir($filedir))
-			{
-				$path = dir($filedir);
-				while(false !== ($entry = $path->read()))
-				{
-					if(($entry != '.') && ($entry != '..'))
-					{           
-						echo "<a href='download.php?number={$post_num}&name={$entry}'>{$entry}</a><br>";
-					}
-				}
-			}
-		?>
-	</div>
-	<div>
-		<h4>댓글</h4>
-		<?php
-			mysqli_stmt_close($stmt);
-
-			$stmt = mysqli_stmt_init($conn);
-			mysqli_stmt_prepare($stmt, "SELECT * FROM comment WHERE post_num = ? ORDER BY comment_num ASC");
-			mysqli_stmt_bind_param($stmt, 'i', $post_num);
-			mysqli_stmt_execute($stmt);
-			$comment_result = mysqli_stmt_get_result($stmt);
-
-			if(isset($_SESSION['user_id']))
-			{
-				$comment_form = <<< HERE
-				<div>
-				<form action="write_comment.php?number={$post_num}" method="post">
-				<textarea name="comment" cols="40" rows="5" placeholder="댓글"></textarea><br>
-				<button type="submit">댓글 쓰기</button>
-				</form>
-				</div>
-				HERE;
-
-				echo $comment_form;
-			}
-			else
-			{
-				$comment_form = <<< HERE
-				<div>
-				<textarea name="comment" cols="40" rows="5" placeholder="댓글 기능을 사용하려면 로그인하세요." readonly="readonly"></textarea><br>
-				<button type="button" onclick="location.href='login.html'">로그인 하기</button>
-				</div>
-				HERE;
-
-				echo $comment_form;
-			}
-		?>
-		<div>
-			<hr>
-			<?php
-				while($row = mysqli_fetch_assoc($comment_result))
-				{
-					if(strcmp($row['user_id'], $_SESSION['user_id']))
-					{
-						$heredoc = <<< HERE
-						<span>작성자: <strong>{$row['user_name']}</strong></span><br>
-						<textarea cols="40" rows="5" readonly="readonly">{$row['comment']}</textarea><br>
-						<span>작성일시: {$row['commented']}</span>
-						<hr>
-						HERE;
-	
-						echo $heredoc;
-					}
-					else
-					{
-						$heredoc = <<< HERE
-						<span>작성자: <strong>{$row['user_name']}</strong></span><br>
-						<form id="form#{$row['comment_num']}" action="edit_comment.php?number={$row['comment_num']}" method="post">
-						<button type="button" onclick="location.href='delete_comment.php?number={$row['comment_num']}'">삭제</button>
-						<button type="button" id="edit_comment#{$row['comment_num']}" onclick="comment_edit(this.id)">수정</button><br>
-						<textarea id="text#{$row['comment_num']}" name="comment" cols="40" rows="5" readonly="readonly">{$row['comment']}</textarea>
-						</form>
-						<span>작성일시: {$row['commented']}</span>
-						<hr>
-						HERE;
-	
-						echo $heredoc;
-					}
-				}
-			?>
-		</div>
 	</div>
 </body>
 </html>
