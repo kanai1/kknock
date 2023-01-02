d3f2fd130f3687d325a0fcc904af2d40f4e3394a
kanai <34500707+kanai1@users.noreply.github.com>
2023-01-02 18:39:10 +0900

    Update signin.php

--- signin.php
+++ signin.php
@@ -14,7 +14,7 @@
 			$password = $_POST['txtPassword'];
 
 			$conn = new mysqli('localhost', 'kknock', 'kknock', 'test');
-			$Stmt = $stmt->prepare("SELECT user_name, login_id FROM user_login WHERE login_id=? && login_pw=?");
+			$Stmt = $conn->prepare("SELECT user_name, login_id FROM user_login WHERE login_id=? && login_pw=?");
 			$stmt->bind_param("ss", $id, $password);
 			$stmt->execute();
 
