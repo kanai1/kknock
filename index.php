ee7a7e971fcc6101a0aedfad9fadb9d2ae4129c3
kanai <34500707+kanai1@users.noreply.github.com>
2023-01-04 16:56:13 +0900

    Update index.php

--- index.php
+++ index.php
@@ -70,11 +70,13 @@
 					<?php
 						while($row = mysqli_fetch_assoc($result))
 						{
+							$title = htmlspecialchars($row['title'])
+							$user_id = htmlspecialchars($row['user_id'])
 							$heredoc = <<< HERE
 							<tr>
 								<td class="post_num" style="text-align: center;">{$row['post_num']}</td>
-								<td class="post_title"><a href="view.php?number={$row['post_num']}">{$row['title']}</a></td>
-								<td class="user_name" style="text-align: center;">{$row['user_id']}</td>
+								<td class="post_title"><a href="view.php?number={$row['post_num']}">{$title}</a></td>
+								<td class="user_name" style="text-align: center;">{$user_id}</td>
 								<td class="post_time">{$row['posted']}</td>
 							</tr>
 							HERE;
